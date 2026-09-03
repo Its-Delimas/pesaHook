ac6fedf3dcb9d6a5f4a7cd716c55fe21acc14791
Its-Delimas <spencerdelimas@gmail.com>
2026-09-03 22:49:53 +0300

    refactor(store): reorganize imports and format NewPostgresAPIKeyStore function

--- pesahook-backend/internal/store/postgres_apikey.go
+++ pesahook-backend/internal/store/postgres_apikey.go
@@ -1,11 +1,26 @@
 package store
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	"github.com/Its-Delimas/pesahook/internal/apikey"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type PostgresAPIKeyStore struct {
 	pool *pgxpool.Pool
 }
 
-func NewPostgresAPIKeyStore (pool *pgxpool.Pool) *PostgresAPIKeyStore {
+func NewPostgresAPIKeyStore(pool *pgxpool.Pool) *PostgresAPIKeyStore {
 	return &PostgresAPIKeyStore{pool: pool}
+}
+
+func (s *PostgresAPIKeyStore) Save(k apikey.APIKey) error {
+	_, err := s.pool.Exec(context.Background(), `
+		INSERT INTO api_keys (id,account_id,key_hash,created_at)
+		VALUES ($1,$2,$3,$4)
+		ON CONFLICT (id) DO NOTHING`,
+		k.ID, k.AccountID, k.KeyHash, k.CreatedAt,
+	)
+	return err
 }
