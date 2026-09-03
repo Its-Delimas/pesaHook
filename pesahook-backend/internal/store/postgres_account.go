37b64ab012f70d0dda31324d34d74dd2c44f4cc4
Its-Delimas <spencerdelimas@gmail.com>
2026-09-03 22:40:42 +0300

    feat(store): implement Save method for PostgresAccountStore to handle account insertion

--- pesahook-backend/internal/store/postgres_account.go
+++ pesahook-backend/internal/store/postgres_account.go
@@ -1,6 +1,11 @@
 package store
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	"github.com/Its-Delimas/pesahook/internal/account"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type PostgresAccountStore struct {
 	pool *pgxpool.Pool
@@ -8,4 +13,13 @@ type PostgresAccountStore struct {
 
 func NewPostgresAccountStore(pool *pgxpool.Pool) *PostgresAccountStore {
 	return &PostgresAccountStore{pool: pool}
+}
+
+func (s *PostgresAccountStore) Save(a account.Account) error {
+	_, err := s.pool.Exec(context.Background(), `
+	INSERT INTO accounts (id, email, created_at)
+	VALUES ($1,$2,$3)
+	ON CONFLICT (id) DO NOTHING`,
+		a.ID, a.Email, a.CreatedAt)
+	return err
 }
